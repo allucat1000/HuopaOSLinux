30d8135f89cfa4d781328039cb164437c745d1ee
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-10-17 00:29:29 +0300

    arch equivalent of install script
    bleh

---
+++ arch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Ensure script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root."
+  exit 1
+fi
+
+USERNAME="huopaos"
+
+# Update system and install packages
+pacman -Syu --noconfirm
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xset xorg-xprop chromium openbox
+
+# Setup .xinitrc for the user
+cat <<EOF > /home/$USERNAME/.xinitrc
+#!/bin/bash
+sleep 1
+exec openbox-session &
+exec chromium --force-dark-mode --kiosk --force-device-scale-factor=0.75 https://allucat1000.github.io/HuopaOS
+EOF
+chown $USERNAME:$USERNAME /home/$USERNAME/.xinitrc
+chmod +x /home/$USERNAME/.xinitrc
+
+# Setup auto-login on tty1
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+cat <<EOF > /etc/systemd/system/getty@tty1.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin $USERNAME --noclear %I \$TERM
+EOF
+
+# Auto-start X on login
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> /home/$USERNAME/.bash_profile
+chown $USERNAME:$USERNAME /home/$USERNAME/.bash_profile
+
+echo "Setup complete. Reboot to start in Chromium kiosk mode."
